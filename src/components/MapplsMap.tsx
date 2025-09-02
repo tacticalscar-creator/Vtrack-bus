b82281b68a277ee2bacbf943d4cb37392b7773f6
tacticalscar-creator <tacticalscar@gmail.com>
2025-09-02 21:02:11 +0530

    Update MapplsMap.tsx

--- src/components/MapplsMap.tsx
+++ src/components/MapplsMap.tsx
@@ -1,26 +1,32 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
-declare global {
-  interface Window {
-    L: any;
-  }
+interface MapplsMapProps {
+  apiKey: string;
 }
 
-function MapplsMap() {
+const MapplsMap: React.FC<MapplsMapProps> = ({ apiKey }) => {
   useEffect(() => {
-    if (window.L) {
-      const map = new window.L.Map("map", {
-        center: [28.61, 77.23], // Delhi coords
-        zoom: 12,
-      });
+    // Dynamically load Mappls SDK
+    const script = document.createElement('script');
+    script.src = `https://apis.mappls.com/advancedmaps/api/${apiKey}/map_sdk.js`;
+    script.async = true;
+    script.onload = () => {
+      if (window.L) {
+        const map = new window.L.Map('map', {
+          center: [28.61, 77.23], // Delhi coords
+          zoom: 12,
+        });
 
-      window.L.marker([28.61, 77.23])
-        .addTo(map)
-        .bindPopup("Delhi - Mappls API Connected");
-    }
-  }, []);
+        window.L.marker([28.61, 77.23])
+          .addTo(map)
+          .bindPopup('Delhi - Mappls API Connected')
+          .openPopup();
+      }
+    };
+    document.body.appendChild(script);
+  }, [apiKey]);
 
-  return <div id="map" style={{ height: "400px", width: "100%" }} />;
-}
+  return <div id="map" style={{ height: '500px', width: '100%' }} />;
+};
 
 export default MapplsMap;
